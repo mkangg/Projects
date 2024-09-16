c9ce9d61195676c714b57473b570c7cb76e76832
mkang8 <mkang8@ncsu.edu>
2024-09-16 17:24:59 -0400

    [Implementation] Handling user input using curses.h

--- snake_c/snake_game.c
+++ snake_c/snake_game.c
@@ -1,3 +1,12 @@
+/**
+ * This is a work in progress Snake Game written in C. 
+ * It uses a row, col system to represent a board environment for the snake to move around in. 
+ * This implementation utilizes the curses library which allows user input to be read immediately
+ * and the game to be printed in a nice format. 
+ * 
+ * Author: Minji Kang
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,9 +14,10 @@
 #include <malloc.h>
 #include <stdbool.h>
 #include <curses.h>
-// #include <conio.h>
 
+// Size of the game board
 #define SIZE 20
+// Constants to keep track of the snake direction 
 #define UP 0
 #define DOWN 1
 #define LEFT 2
@@ -28,88 +38,93 @@ bool game;
 // semaphore to control game board
 sem_t sem;
 
-// typedef struct coordinates {
-//     int x;
-//     int y;
-//     bool head;
-//     bool tail;
-//     struct coordinates *next;
-// } coords;
-
+// Using a struct to keep track of "coordinates" or locations of the board
 typedef struct coordinates {
     int row;
     int col;
 } coords;
 
-// // head of the snake 
-// coords head;
-
-// // tail of the snake
-// coords tail;
-
-// // keeping track of snake
-// coords **snake;
-
-void print() {
-    system( "clear" );
+/**
+ * Function that prints out the board. Gets called every second to update the screen. 
+ */
+void printGame() {
+    // Clearing the screen
+    erase();
+    // Checking that the game is not over
     if ( game ) {
+        // Printing the top row of *
         for ( int i = 0; i < SIZE + 2; i++ ) {
-            printf( "%s", "* " );
+            printw( "%s", "* " );
         }
-        printf( "%c", '\n' );
+        printw( "%c", '\n' );
+        // Printing the current state of the game board
         for ( int i = 0; i < SIZE; i++ ) {
-            printf( "%s", "* " );
+            printw( "%s", "* " );
             for ( int j = 0; j < SIZE; j++ ) {
+                // Checking for snake parts and printing as appropriate
                 if ( !board[ i ][ j ] || board[ i ][ j ] == 0 ) {
-                    printf( "%s", "  " );
+                    printw( "%s", "  " );
                 } else {
-                    printf( "%c ", board[ i ][ j ] );
+                    printw( "%c ", board[ i ][ j ] );
                 }
             }
-            printf( "%s", "*\n" );
+            printw( "%s", "*\n" );
         }
+        // Printing bottom row of *
         for ( int i = 0; i < SIZE + 2; i++ ) {
-            printf( "%s", "* " );
+            printw( "%s", "* " );
         }
-        printf( "%c", '\n' );
+        printw( "%c", '\n' );
+        // Refreshing window to reveal the current board to user
+        refresh();
     } else {
+        // Game is over, closing window and printing score
+        endwin(); 
         printf( "%s%d%s", "GAME OVER\nScore: ", length, "\n" );
     }
 }
 
+/**
+ * Since the board was malloc'ed, it needs to be freed
+ */
 void freeBoard() {
     for ( int i = 0; i < SIZE; i++ ) {
         free( board [ i ] );
     }
     free( board ); 
-    // free( snake );
 }
 
-// void moveSnake() {
-
-// }
-
+/**
+ * Main function that deals with the bulk of the game
+ */
 int main() {
+    // Getting memory for the board
     board = malloc( sizeof( char * ) * SIZE );
     for ( int i = 0; i < SIZE; i++ ) {
         board[ i ] = calloc( SIZE, sizeof( char ) );
     }
-    // snake = malloc( sizeof( coords ) * SIZE * SIZE );
+    // Initializing variables needed to start
     length = 1;
     direction = RIGHT;
-    // coords *head = {9, 9, true, true, NULL };
-    // snake = head;
     coords head = { 9, 9 };
     coords tail = { 9, 9 };
     board[ head.row ][ head.col ] = 'O';
-    char command;
-    // WINDOW *scr = initscr();
-    // cbreak();
-    nodelay( stdscr, TRUE );
+    int command = -1;
+    // Initializing the stdscr which allows for a cleaner user experience
+    initscr();
+    // Does not need to echo user input
+    noecho();
+    // Read in user input right away
+    nodelay( stdscr, true );
+    // Allows arrow keys to be processed
+    keypad( stdscr, true );
     game = true;
     while( game ) {
+        // sleeping for a second
         sleep( 1 );
-        if ( ( command = getch() ) != -1 ) {
+        // Getting user command
+        if ( ( command = wgetch( stdscr ) ) != -1 ) {
+            printf( "%c", command );
             if ( command == 'q' ) {
                 game = false;
                 break;
@@ -126,8 +141,8 @@ int main() {
                 direction = RIGHT;
             }
         }
-        // int prev_row, prev_y;
-        if ( head.row > SIZE - 2 || head.col > SIZE - 2 ) {
+        // Seeing if the snake is within bounds
+        if ( head.row > SIZE - 2 || head.col > SIZE - 2 || head.row < 2 || head.col < 2 ) {
             game = false;
         } else if ( direction == UP ) {
             if ( board[ head.row - 1 ][ head.col ] == '@' ) {
@@ -139,10 +154,6 @@ int main() {
                 game = false;
                 sem_post( &sem );
             }
-            // current = current->nerowt;
-            // for ( int i = 0; current && i < length; i++ ) {
-            //     board[ current.x ][ current.y ] = 
-            // }
             board[ --head.row ][ head.col ] = 'O';
             board[ tail.row-- ][ tail.col ] = 0;
         } else if ( direction == DOWN ) {
@@ -182,8 +193,8 @@ int main() {
             board[ head.row ][ ++head.col ] = 'O';
             board[ tail.row ][ tail.col++ ] = 0;
         }
-        print();
-        // sleep( 2 );
+        printGame();
     }
     freeBoard();
+    return 0;
 }
