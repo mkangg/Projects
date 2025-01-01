990ef950ffc9113c4a5878a9275b1c71ef36184a
mkang8 <mkang8@ncsu.edu>
2025-01-01 11:10:57 -0500

    [Feature] Player names added

--- sparring_counter/sparring_counter_flask.py
+++ sparring_counter/sparring_counter_flask.py
@@ -1,20 +1,37 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, redirect, url_for
 
 class SparringCounter:
     def __init__(self):
         self.red = False
         self.blue = False
+        self.redName = ""
+        self.blueName = ""
         
     def reset(self):
         self.red = False
         self.blue = False
+        self.redName = ""
+        self.blueName = ""
         
     def todict(self):
-        return {'red':self.red, 'blue':self.blue}
+        return {'red':self.red, 'blue':self.blue, 'redName':self.redName, 'blueName':self.blueName}
         
 
 app = Flask(__name__)
 sc = SparringCounter()
+
+@app.route('/setup', methods=['GET', 'POST'])
+def setup():
+    if request.method == "POST":
+        return redirect(url_for('counterView'))
+    return render_template('setup.html')
+
+@app.route('/names', methods = ['GET', 'POST'])
+def names():
+    if request.method == 'POST': 
+        sc.redName = request.get_json().get('redName')
+        sc.blueName = request.get_json().get('blueName')
+    return jsonify(sc.todict())
 
 @app.route('/counter/view', methods = ['GET'])
 def counterView():
